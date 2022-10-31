3c9efabfe013a84ac24f5b7c0c7136c4a893b7c4
deniswsviana <denisviana1055@gmail.com>
2022-10-31 15:26:43 -0300

    projeto-leilao

---
+++ Site/js/times/timer.js
@@ -0,0 +1,28 @@
+function starTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var ranTimer = Math.floor(Math.random() * (60 - 25) + 25);
+  var duration = 60 * ranTimer;
+  var display = document.querySelector(".timer", ".timer1");
+
+  starTimer(duration, display);
+}
+
